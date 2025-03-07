1d0efc3d43135c7c6aecf6bbcf150f35b4a43fd0
leahcarlin <leahrcarlin@gmail.com>
2025-03-06 21:28:33 -0500

    mobile

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LogoLight from "../../assets/logo_light.png";
 import LogoDark from "../../assets/logo_dark.png";
+import NavBarMobile from "./NavBarMobile";
 import "./NavBar.scss";
 
 const NavBar: React.FC = () => {
   const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  const handleResize = () => setScreenWidth(window.innerWidth);
+
+  useEffect(() => {
+    // set initial value on mount
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    // cleanup on unmount
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <nav>
@@ -13,13 +27,17 @@ const NavBar: React.FC = () => {
       ) : (
         <img src={LogoLight} alt="light" />
       )}
-      <div className="links">
-        <a href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#tech">Tech</a>
-        <a href="#projects">Projects</a>
-        <a href="#contact">Contact</a>
-      </div>
+      {screenWidth < 480 ? (
+        <NavBarMobile />
+      ) : (
+        <div className="links">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#tech">Tech</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
+        </div>
+      )}
     </nav>
   );
 };
